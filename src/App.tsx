98473779ecada41d3d00ba44b49c2a08d8fdfaaf
kelpikz <ajithaprasad96@gmail.com>
2021-06-27 02:43:16 +0530

    INIT PROJECT

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import "tailwindcss/tailwind.css";
+
+function App() {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<div className="bg-gradient-to-r from-blue-400 to-purple-700 text-white text-7xl antialiased  font-semibold rounded-lg border shadow-lg p-10">
+				Research Portal
+			</div>
+		</div>
+	);
+}
+
+export default App;
